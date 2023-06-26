707bf6dc51b1c0f0bfdb2e05d4c137853d0629b1
PaiMax <nishwalbh1997@gmail.com>
2023-06-27 01:12:18 +0530

    multiple value store

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 document.addEventListener('submit', store);
- function store(e){
+
+function store(e){
+   e.preventDefault(); 
    
     
     let myobj= {
@@ -9,7 +11,20 @@ document.addEventListener('submit', store);
 
    }
    let myserial=JSON.stringify(myobj);
-   localStorage.setItem('user',myserial);
+   localStorage.setItem(document.getElementById('email').value,myserial);
+   let list=document.createElement('li');
+   let name=document.createTextNode(document.getElementById('name').value+'-');
+   let email=document.createTextNode(document.getElementById('email').value);
+   console.log(name);
+   console.log(email);
+   
+   
+   
+   
+   list.append(name,email);
+   let li=document.getElementById('users');
+   li.appendChild(list);
+
 
 
 
