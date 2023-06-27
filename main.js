252a845b97137f1383f19561c37d692b95b38fa3
PaiMax <nishwalbh1997@gmail.com>
2023-06-27 15:07:22 +0530

    edit button

--- main.js
+++ main.js
@@ -11,8 +11,11 @@ function store(e){
 
    }
    let butn=document.createElement('input');
+   let edit=document.createElement('input');
    butn.type='Button';
    butn.value='Delete';
+   edit.value='edit';
+   edit.type='Button';
    let myserial=JSON.stringify(myobj);
    localStorage.setItem(document.getElementById('email').value,myserial);
    let list=document.createElement('li');
@@ -20,11 +23,22 @@ function store(e){
    let email=document.createTextNode(document.getElementById('email').value);
    list.append(name,email);
    list.append(butn);
+   list.append(edit);``
    let li=document.getElementById('users');
    li.appendChild(list);
    butn.onclick=deleteuser;
+   edit.onclick=edituser;
    function deleteuser(){
     list.remove();
+    localStorage.removeItem(document.getElementById('email').value);
+   }
+   function edituser(){
+    localStorage.removeItem(document.getElementById('email').value);
+    list.remove();
+    document.getElementById('name').value=myobj.name;
+    document.getElementById('email').value=myobj.email;
+
+
    }
    
 
