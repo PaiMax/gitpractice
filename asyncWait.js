7bea2f57d407fa7b39a9aa5ac1a92f0f337ac82c
PaiMax <nishwalbh1997@gmail.com>
2023-07-06 21:38:45 +0530

    async call

---
+++ asyncWait.js
@@ -0,0 +1,29 @@
+console.log('person1: shows ticket');
+console.log('person2: shows ticket');
+ 
+const premovie = async () =>{
+    const promiseWifeBringingTicks=new Promise((resolve,reject)=>{
+        setTimeout(()=>resolve('ticket'),3000);
+    });
+    const getpopcorn=new Promise((resolve,reject)=> resolve( 'popcorn'));
+    const getbutter=new Promise((resolve,reject)=> resolve('butter' ));
+
+    let ticket= await promiseWifeBringingTicks;
+    console.log('husband: we should go in');
+    console.log('wife: no i am hungry');
+    let popcorn=await getpopcorn;
+    console.log('husband: we got popcorn');
+    console.log('husband: we should go in');
+    console.log('wife: no i need butter');
+    let butter=await getbutter;
+    console.log(` i got some ${butter}`);
+    return ticket;
+
+     
+
+
+
+}
+premovie().then((m)=> console.log(`person3: shows ${m}`));
+console.log('person4: shows ticket');
+console.log('person5: shows ticket');
